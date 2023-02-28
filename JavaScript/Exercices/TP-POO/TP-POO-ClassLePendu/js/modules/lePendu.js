a063d535f822f39c626045b7f4289d1b2e645cfa
senia touaf <cherinalje@hotmail.fr>
2023-02-28 16:55:20 +0100

    exo le pendu

---
+++ JavaScript/Exercices/TP-POO/TP-POO-ClassLePendu/js/modules/lePendu.js
@@ -0,0 +1,10 @@
+export default class lePendu{
+    constructor(motATrouver, masque, nbEssais, dejaPropose,win){
+        this.motATrouver = motATrouver,
+        this.masque = masque ,
+        this.nbEssais = nbEssais,
+        this.dejaPropose = dejaPropose,
+        this.win = win
+        
+    }
+}
